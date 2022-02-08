b04a82688d623c3b1941f0f356213b291ff7c6dc
sanghun1 <pooij96@naver.com>
2022-02-09 03:37:50 +0900

    .

---
+++ app/src/main/java/com/cos/instagram/adapter/ProfileImgAdapter.java
@@ -0,0 +1,71 @@
+package com.cos.instagram.adapter;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.cos.instagram.ProfileImgActivity;
+import com.cos.instagram.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ProfileImgAdapter extends RecyclerView.Adapter<ProfileImgAdapter.ProfileImgHolder>{
+    private ArrayList<String> mDataset;
+    private Activity activity;
+
+    public static class ProfileImgHolder extends RecyclerView.ViewHolder {
+        public CardView cardView;
+        public ProfileImgHolder(CardView v) {
+            super(v);
+            cardView = v;
+        }
+
+    }
+
+    public ProfileImgAdapter(Activity activity, ArrayList<String> myDataset) {
+        mDataset = myDataset;
+        Collections.reverse(mDataset);
+        this.activity = activity;
+    }
+
+    @Override
+    public ProfileImgAdapter.ProfileImgHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView cardView = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_gallery, parent, false);
+
+        return new ProfileImgHolder(cardView);
+    }
+
+    @NonNull
+    @Override
+    public void onBindViewHolder(@NonNull ProfileImgHolder holder, int position) {
+        CardView cardView = holder.cardView;
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(activity, mDataset.get(holder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                Bitmap bmp = BitmapFactory.decodeFile(mDataset.get(holder.getAdapterPosition()));
+                ((ProfileImgActivity)ProfileImgActivity.ImgContext).imgView.setImageBitmap(bmp);
+            }
+        });
+        ImageView imageView = cardView.findViewById(R.id.imageView);
+        Glide.with(activity).load(mDataset.get(position)).centerCrop().override(500).into(imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+
+}
